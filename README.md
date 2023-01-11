bf9e2b5450b154b2bc9c8a2f821d9607cc739cd4
dudvdw <dudvdw@163.com>
2023-01-11 12:00:10 +0800

    project0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-CS 61B note
+# CS 61B note
 
-# Chapter 1 Introdution to Java
+## Chapter 1 Introdution to Java
 
-# Chapter 2 Lists
+## Chapter 2 Lists
--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author TODO: DUDVDW
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                if (b.tile(i, j) == null || b.tile(i, j).value() == 0) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -148,6 +155,13 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                if (b.tile(i, j) != null && b.tile(i, j).value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -159,6 +173,20 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                if (b.tile(i, j) == null) {
+                    return true;
+                } else {
+                        if (j < b.size()-1 && b.tile(i, j).value() == b.tile(i, j+1).value()) {
+                            return true;
+                        }
+                        if (i < b.size()-1 && b.tile(i, j).value() == b.tile(i+1, j).value()) {
+                            return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
