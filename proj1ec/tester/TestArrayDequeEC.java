e35e86b020fb1a5ec278d911b590f2ed60ab23f3
dudvdw <dudvdw@163.com>
2023-02-17 23:17:26 +0800

    proj1EC add msg

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,28 +12,35 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> S = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> A = new ArrayDequeSolution<>();
 
-        int N = 500;
+        int N = 5000;
+        String msg = "";
         for (int i = 0; i < N; i++) {
             int operationNumber = StdRandom.uniform(0, 5);
             if (operationNumber == 0 || A.size() == 0) {
                 int randVal = StdRandom.uniform(0, N);
                 S.addLast(randVal);
                 A.addLast(randVal);
+                msg += "addLast(" + randVal + ")\n";
             } else if (operationNumber == 1) {
                 int randVal = StdRandom.uniform(0, N);
                 S.addFirst(randVal);
                 A.addFirst(randVal);
+                msg += "addFirst(" + randVal + ")\n";
             } else if (operationNumber == 2) {
                 int randIndex = StdRandom.uniform(0, N) % A.size();
-                assertEquals("get(" + randIndex + ")", S.get(randIndex), A.get(randIndex));
+                msg += "get(" + randIndex + ")\n";
+                assertEquals(msg, S.get(randIndex), A.get(randIndex));
             } else if (operationNumber == 3) {
                 int s = S.removeFirst();
                 int a = A.removeFirst();
-                assertEquals("removeFirst()", s, a);
+                msg += "removeFirst()\n";
+
+                assertEquals(msg, s, a);
             } else {
                 int s = S.removeLast();
                 int a = A.removeLast();
-                assertEquals("removeLast()", s, a);
+                msg += "removeLast()\n";
+                assertEquals(msg, s, a);
             }
         }
     }
