4e0db6a10cfecd861de4026234a7e9adbb6073c6
dudvdw <dudvdw@163.com>
2023-02-18 21:44:23 +0800

    add annotation for lab4

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -7,6 +7,11 @@ public class Flik {
     /** @param a Value 1
      *  @param b Value 2
      *  @return Whether a and b are the same */
+
+    // Java create an IntegerCache from -128 to 127,
+    // when you create an Integer in this range, it will refer to the IntegerCache,
+    // when the Integer out of range, it will create a new Integer in a new address
+    // each time.
     public static boolean isSameNumber(int a, int b) {
         return a == b;
     }
